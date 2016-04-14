a0b3330004d00242c30eaa7cd6a504cec7b8f489
Eugenu <maximys88888@gmail.com>
2016-04-15 02:38:27 +0300

    Added methods for WallDecorator

--- poporo-15B3306-15B3517-15B3208/src/innopolis/poporo/WallDecorator.java
+++ poporo-15B3306-15B3517-15B3208/src/innopolis/poporo/WallDecorator.java
@@ -15,5 +15,20 @@ public class WallDecorator extends Decorator {
 		super.Draw();
 		DrawWall();
 	}
-	
+
+	public void comment(ContentItem c1, ContentItem c2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isDefinedAt(ContentItem c1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean has(ContentItem c1, ContentItem c2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
