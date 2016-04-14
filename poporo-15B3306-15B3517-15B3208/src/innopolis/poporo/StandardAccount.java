3f1914c3fc199ed1b3f5ba24879b9317ae2b7c5a
Eugenu <maximys88888@gmail.com>
2016-04-15 01:50:03 +0300

    Fix Account

--- poporo-15B3306-15B3517-15B3208/src/innopolis/poporo/StandardAccount.java
+++ poporo-15B3306-15B3517-15B3208/src/innopolis/poporo/StandardAccount.java
@@ -2,32 +2,38 @@ package innopolis.poporo;
 
 public class StandardAccount extends Account
 {
-	private StandardUser user;
-	private StandardPage page;
-	
-	public StandardAccount(){
-		this.page=new StandardPage();
-	}
+    private StandardUser user;
+    private StandardPage page;
+    
+    public StandardAccount()
+    {
+        this.page = new StandardPage();
+    }
 
-	@Override
-	public StandardUser openAccount() {
-		this.user=new StandardUser();
-		return this.user;
-	}
+    @Override
+    public StandardUser openAccount()
+    {
+        this.user = new StandardUser();
+        System.out.println("StandardAccount has been opened.");  
+        return this.user;
+    }
 
-	@Override
-	public void transmit(ContentItem ci) {
-		this.page.upload(ci);
-	}
+    @Override
+    public void transmit(ContentItem ci)
+    {
+        page.upload(ci);
+    }
 
-	@Override
-	public void closeAccount() {
-		System.out.println("Account was closed.");	
-	}
+    @Override
+    public void closeAccount()
+    {
+        System.out.println("StandardAccount has been closed.");  
+    }
 
-	@Override
-	public boolean contains(ContentItem c) {
-		return this.page.containsContentItem(c);
-	}
+    @Override
+    public boolean contains(ContentItem c)
+    {
+        return this.page.containsContentItem(c);
+    }
 
 }
