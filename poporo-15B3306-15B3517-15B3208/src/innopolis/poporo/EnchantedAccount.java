28b6c271e98562437bc64955b04450662c8cdb62
somal <somal1996@rambler.ru>
2016-04-15 01:31:45 +0300

    Exercise 6 is finished

--- poporo-15B3306-15B3517-15B3208/src/innopolis/poporo/EnchantedAccount.java
+++ poporo-15B3306-15B3517-15B3208/src/innopolis/poporo/EnchantedAccount.java
@@ -2,27 +2,32 @@ package innopolis.poporo;
 
 public class EnchantedAccount extends Account
 {
-	public EnchantedAccount()
-	{
-		
+	private EnchantedUser user;
+	private EnchantedPage page;
+	
+	public EnchantedAccount(){
+		this.page=new EnchantedPage();
 	}
 
 	@Override
-	public void openAccount() {
-		// TODO Auto-generated method stub
-		
+	public EnchantedUser openAccount() {
+		this.user=new EnchantedUser();
+		return this.user;
 	}
 
 	@Override
 	public void transmit(ContentItem ci) {
-		// TODO Auto-generated method stub
-		
+		this.page.upload(ci);
 	}
 
 	@Override
 	public void closeAccount() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Account was closed.");		
+	}
+
+	@Override
+	public boolean contains(ContentItem c) {
+		return this.page.containsContentItem(c);
 	}
 
 }
--- poporo-15B3306-15B3517-15B3208/src/innopolis/poporo/StandardAccount.java
+++ poporo-15B3306-15B3517-15B3208/src/innopolis/poporo/StandardAccount.java
@@ -2,23 +2,32 @@ package innopolis.poporo;
 
 public class StandardAccount extends Account
 {
+	private StandardUser user;
+	private StandardPage page;
+	
+	public StandardAccount(){
+		this.page=new StandardPage();
+	}
 
 	@Override
-	public void openAccount() {
-		// TODO Auto-generated method stub
-		
+	public StandardUser openAccount() {
+		this.user=new StandardUser();
+		return this.user;
 	}
 
 	@Override
 	public void transmit(ContentItem ci) {
-		// TODO Auto-generated method stub
-		
+		this.page.upload(ci);
 	}
 
 	@Override
 	public void closeAccount() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Account was closed.");	
+	}
+
+	@Override
+	public boolean contains(ContentItem c) {
+		return this.page.containsContentItem(c);
 	}
 
 }
