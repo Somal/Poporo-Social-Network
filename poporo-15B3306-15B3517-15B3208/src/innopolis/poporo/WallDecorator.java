3c5dc2e4c885f7f236348fc1e51f73ea384cbf2b
somal <somal1996@rambler.ru>
2016-04-15 01:50:20 +0300

    Create WallDecorator class

---
+++ poporo-15B3306-15B3517-15B3208/src/innopolis/poporo/WallDecorator.java
@@ -0,0 +1,49 @@
+package innopolis.poporo;
+
+public class WallDecorator extends Page {
+
+	public WallDecorator(Page ep){
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public void upload(ContentItem c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean remove(ContentItem c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmptyPage() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsContentItem(ContentItem c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void comment(ContentItem c1, ContentItem c2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isDefinedAt(ContentItem c1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean has(ContentItem c1, ContentItem c2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
